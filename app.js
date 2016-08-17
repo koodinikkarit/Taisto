f0ac17ff235fe213f7f2bd646aab8f5facfca864
Jaska <aa>
2016-08-17 22:47:13 +0300

    Testiä app.js tiedostossa.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var Matrix = require("./Matti.js/Matrix");
 var manager = require("./Matti.js/MattiManager")("10.1.1.10", "matti", "jaska");
 
+manager.createMatrix("192.168.180.98", 5555, 16, 16);
 
-manager.createToken("kameli38", function (token) {
-	console.log("token lutu");
+manager.createAdmin("kameli38", function (token) {
 	token.createProfile("oletus", function (profile) {
-
+		profile.getMatrixs(function (matrixs) {
+			//console.log(matrixs[0]);
+			//console.log(matrixs[0].makeVideoConnection());
+			matrixs[0].makeVideoConnection(3,12);
+		});
 	});
 });
